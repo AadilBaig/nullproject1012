f158702ddd5e29606c222d79342bb14e5e10718b
rayko-v <91995284+rayko-v@users.noreply.github.com>
2021-12-04 20:13:20 -0500

    Delete null_2.js

--- null project/js/null_2.js
+++
@@ -1,6 +0,0 @@
-var value;
-function start1() {
-  value = document.querySelector('input[name="difficulty"]:checked').value;
-  sessionStorage.setItem("choiceValue", value)
-  window.location.href = `../html/null_3.html`;
-}
